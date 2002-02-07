fa52c73de776d7c66e83ba44ef31b3b7c707d8a4
Enver Haase <enver@apache.org>
2002-02-07 23:12:59 +0000

    updated URL of JustICE PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/bcel/trunk@152734 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/bcel/verifier/VerifierAppFrame.java
+++ src/java/org/apache/bcel/verifier/VerifierAppFrame.java
@@ -378,7 +378,7 @@ public class VerifierAppFrame extends JFrame {
 
   void aboutMenuItem_actionPerformed(ActionEvent e) {
     JOptionPane.showMessageDialog(this,
-            "JustIce is a Java class file verifier.\nIt was implemented by Enver Haase in 2001.\nhttp://bcel.sourceforge.net",
+            "JustIce is a Java class file verifier.\nIt was implemented by Enver Haase in 2001, 2002.\n<http://jakarta.apache.org/bcel/index.html>",
              JUSTICE_VERSION, JOptionPane.INFORMATION_MESSAGE);
   }
 
