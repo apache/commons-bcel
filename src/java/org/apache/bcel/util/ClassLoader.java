7353b042d83025f3e9d346e35b4215baca604016
David Dixon-Peugh <ddp@apache.org>
2002-05-10 14:45:56 +0000

    Fixed:  Was using the SystemClassLoader when loading a class that is ignored by the ClassLoader.  Now, it uses "deferTo".
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/bcel/trunk@152784 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/bcel/util/ClassLoader.java
+++ src/java/org/apache/bcel/util/ClassLoader.java
@@ -89,23 +89,42 @@ public class ClassLoader extends java.lang.ClassLoader {
   private String[] ignored_packages = {
     "java.", "javax.", "sun."
   };
-    private Repository repository = null;
+    private Repository repository = 
+	SyntheticRepository.getInstance();
+    private java.lang.ClassLoader deferTo = ClassLoader.getSystemClassLoader();
 
   public ClassLoader() {
   }
+
+    public ClassLoader( java.lang.ClassLoader deferTo ) {
+	this.deferTo = deferTo;
+	this.repository = new ClassLoaderRepository( deferTo );
+    }
 
   /** @param ignored_packages classes contained in these packages will be loaded
    * with the system class loader
    */
   public ClassLoader(String[] ignored_packages) {
-    String[] new_p = new String[ignored_packages.length + this.ignored_packages.length];
-
-    System.arraycopy(this.ignored_packages, 0, new_p, 0, this.ignored_packages.length);
-    System.arraycopy(ignored_packages, 0, new_p, this.ignored_packages.length,
-		     ignored_packages.length);
-
-    this.ignored_packages = new_p;
+      addIgnoredPkgs( ignored_packages );
   }
+
+    public ClassLoader( java.lang.ClassLoader deferTo, 
+			String [] ignored_packages ) {
+	this.deferTo = deferTo;
+	this.repository = new ClassLoaderRepository( deferTo );
+
+	addIgnoredPkgs( ignored_packages );
+    }
+
+    private void addIgnoredPkgs( String[] ignored_packages ) {
+	String[] new_p = new String[ignored_packages.length + this.ignored_packages.length];
+	
+	System.arraycopy(this.ignored_packages, 0, new_p, 0, this.ignored_packages.length);
+	System.arraycopy(ignored_packages, 0, new_p, this.ignored_packages.length,
+			 ignored_packages.length);
+
+	this.ignored_packages = new_p;
+    }
   
   protected Class loadClass(String class_name, boolean resolve) 
     throws ClassNotFoundException 
@@ -120,7 +139,7 @@ public class ClassLoader extends java.lang.ClassLoader {
        */
       for(int i=0; i < ignored_packages.length; i++) {
 	if(class_name.startsWith(ignored_packages[i])) {
-	  cl = Class.forName(class_name);
+	  cl = deferTo.loadClass(class_name);
 	  break;
 	}
       }
@@ -133,8 +152,9 @@ public class ClassLoader extends java.lang.ClassLoader {
 	if(class_name.indexOf("$$BCEL$$") >= 0)
 	  clazz = createClass(class_name);
 	else { // Fourth try: Load classes via repository
-	  if((clazz = org.apache.bcel.Repository.lookupClass(class_name)) != null)
-	    clazz = modifyClass(clazz);
+	    if ( (clazz = repository.loadClass( class_name )) != null) {
+		clazz = modifyClass( clazz );
+	    }
 	  else
 	    throw new ClassNotFoundException(class_name);
 	}
