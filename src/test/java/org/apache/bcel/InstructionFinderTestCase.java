7df92b36952cb730e139b6b79e9829c2349a54cc
David Brosius <dbrosius@apache.org>
2007-06-23 17:47:38 +0000

    Add test case to show that InstructionFinder.search returns one more value than supposed to, issue found by Nicholson, Jonathan O H
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/bcel/trunk@550066 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/bcel/InstructionFinderTestCase.java
@@ -0,0 +1,45 @@
+package org.apache.bcel;
+
+import java.util.Iterator;
+
+import org.apache.bcel.AbstractTestCase;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.Method;
+import org.apache.bcel.generic.InstructionList;
+import org.apache.bcel.util.InstructionFinder;
+import org.apache.bcel.util.InstructionFinder.CodeConstraint;
+
+public class InstructionFinderTestCase extends AbstractTestCase
+{
+	public void testSearchAll() throws Exception
+	{
+		JavaClass clazz = getTestClass("org.apache.bcel.util.InstructionFinder");
+		Method[] methods = clazz.getMethods();
+		Method searchM = null;
+		for (Method m : methods)
+		{
+			if (m.getName().equals("search") && (m.getArgumentTypes().length == 3))
+			{
+				searchM = m;
+				break;
+			}
+		}
+		
+		if (searchM == null)
+			throw new Exception("search method not found");
+		
+		byte[] bytes = searchM.getCode().getCode();
+		InstructionList il = new InstructionList(bytes);
+		InstructionFinder finder = new InstructionFinder(il);
+		Iterator it = finder.search(".*", il.getStart(), null);
+		
+		int cnt = 0;
+		while (it.hasNext())
+		{
+			it.next();
+			cnt++;
+		}
+		
+		assertEquals(1, cnt);
+	}
+}
