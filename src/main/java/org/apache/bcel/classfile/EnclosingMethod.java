115499dd09fb182589c16521761bdc84ded4fd5d
David Brosius <dbrosius@apache.org>
2010-05-06 16:43:17 +0000

    Apply patch from Bug 49259 to relicense two files to Apache 2.0 from IBM as provided from GSOC student.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/bcel/trunk@941798 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/bcel/classfile/EnclosingMethod.java
+++ src/main/java/org/apache/bcel/classfile/EnclosingMethod.java
@@ -1,18 +1,22 @@
-/* *******************************************************************
- * Copyright (c) 2004 IBM Corporation
- * 
- * All rights reserved. 
- * This program and the accompanying materials are made available 
- * under the terms of the Common Public License v1.0 
- * which accompanies this distribution and is available at 
- * http://www.eclipse.org/legal/cpl-v10.html 
- *  
- * Contributors: 
- *    Andy Clement     initial implementation 
- * ******************************************************************/
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.apache.bcel.classfile;
 
-import java.io.DataInput;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
@@ -39,7 +43,7 @@ public class EnclosingMethod extends Attribute {
 	private int methodIndex;
 
 	// Ctors - and code to read an attribute in.
-	public EnclosingMethod(int nameIndex, int len, DataInput dis, ConstantPool cpool) throws IOException {
+	public EnclosingMethod(int nameIndex, int len, DataInputStream dis, ConstantPool cpool) throws IOException {
 		this(nameIndex, len, dis.readUnsignedShort(), dis.readUnsignedShort(), cpool);
 	}
 
--- src/main/java/org/apache/bcel/classfile/LocalVariableTypeTable.java
+++ src/main/java/org/apache/bcel/classfile/LocalVariableTypeTable.java
@@ -1,18 +1,20 @@
-/* *******************************************************************
- * Copyright (c) 2004 IBM Corporation
- * 
- * All rights reserved. 
- * This program and the accompanying materials are made available 
- * under the terms of the Common Public License v1.0 
- * which accompanies this distribution and is available at 
- * http://www.eclipse.org/legal/cpl-v10.html 
- *  
- * Contributors: 
- *    Andy Clement     initial implementation 
- *    Heavily based on LocalVariableTable
- * ******************************************************************/
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.apache.bcel.classfile;
-
 
 import  org.apache.bcel.Constants;
 import  java.io.*;
@@ -106,7 +108,7 @@ public class LocalVariableTypeTable extends Attribute {
    * @return String representation.
    */ 
   public final String toString() {
-    StringBuffer buf = new StringBuffer();
+    StringBuffer buf = new StringBuffer("");
 
     for(int i=0; i < local_variable_type_table_length; i++) {
       buf.append(local_variable_type_table[i].toString());
