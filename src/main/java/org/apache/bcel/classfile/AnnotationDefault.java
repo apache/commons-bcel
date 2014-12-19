f9b81a07317f588f93a101ad3398d476a7b353b9
Emmanuel Bourg <ebourg@apache.org>
2014-12-19 13:19:42 +0000

    Reformatted AnnotationDefault
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/bcel/trunk@1646703 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/bcel/classfile/AnnotationDefault.java
+++ src/main/java/org/apache/bcel/classfile/AnnotationDefault.java
@@ -13,103 +13,85 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *
  */
+
 package org.apache.bcel.classfile;
 
 import java.io.DataInput;
 import java.io.DataOutputStream;
 import java.io.IOException;
+
 import org.apache.bcel.Constants;
 
 /**
- * represents the default value of a annotation for a method info
- * 
- * @version $Id: AnnotationDefault 1 2005-02-13 03:15:08Z dbrosius $
+ * Represents the default value of a annotation for a method info
+ *
  * @author <A HREF="mailto:dbrosius@qis.net">D. Brosius</A>
+ * @version $Id: AnnotationDefault 1 2005-02-13 03:15:08Z dbrosius $
  * @since 6.0
  */
-public class AnnotationDefault extends Attribute
-{
-        private static final long serialVersionUID = -4017327188724019487L;
+public class AnnotationDefault extends Attribute {
+    
+    private static final long serialVersionUID = -4017327188724019487L;
 
-        private ElementValue default_value;
+    private ElementValue default_value;
 
-        /**
-         * @param name_index
-         *            Index pointing to the name <em>Code</em>
-         * @param length
-         *            Content length in bytes
-         * @param input
-         *            Input stream
-         * @param constant_pool
-         *            Array of constants
-         */
-        AnnotationDefault(int name_index, int length, DataInput input, ConstantPool constant_pool) throws IOException
-        {
-                this(name_index, length, (ElementValue) null,
-                                constant_pool);
-                default_value = ElementValue.readElementValue(input, constant_pool);
-        }
+    /**
+     * @param name_index    Index pointing to the name <em>Code</em>
+     * @param length        Content length in bytes
+     * @param input         Input stream
+     * @param constant_pool Array of constants
+     */
+    AnnotationDefault(int name_index, int length, DataInput input, ConstantPool constant_pool) throws IOException {
+        this(name_index, length, (ElementValue) null, constant_pool);
+        default_value = ElementValue.readElementValue(input, constant_pool);
+    }
 
-        /**
-         * @param name_index
-         *            Index pointing to the name <em>Code</em>
-         * @param length
-         *            Content length in bytes
-         * @param defaultValue
-         *            the annotation's default value
-         * @param constant_pool
-         *            Array of constants
-         */
-        public AnnotationDefault(int name_index, int length,
-                        ElementValue defaultValue, ConstantPool constant_pool)
-        {
-                super(Constants.ATTR_ANNOTATION_DEFAULT, name_index, length, constant_pool);
-                setDefaultValue(defaultValue);
-        }
+    /**
+     * @param name_index    Index pointing to the name <em>Code</em>
+     * @param length        Content length in bytes
+     * @param defaultValue  the annotation's default value
+     * @param constant_pool Array of constants
+     */
+    public AnnotationDefault(int name_index, int length, ElementValue defaultValue, ConstantPool constant_pool) {
+        super(Constants.ATTR_ANNOTATION_DEFAULT, name_index, length, constant_pool);
+        setDefaultValue(defaultValue);
+    }
 
-        /**
-         * Called by objects that are traversing the nodes of the tree implicitely
-         * defined by the contents of a Java class. I.e., the hierarchy of methods,
-         * fields, attributes, etc. spawns a tree of objects.
-         * 
-         * @param v
-         *            Visitor object
-         */
-        @Override
-        public void accept(Visitor v)
-        {
-                v.visitAnnotationDefault(this);
-        }
+    /**
+     * Called by objects that are traversing the nodes of the tree implicitely
+     * defined by the contents of a Java class. I.e., the hierarchy of methods,
+     * fields, attributes, etc. spawns a tree of objects.
+     *
+     * @param v Visitor object
+     */
+    @Override
+    public void accept(Visitor v) {
+        v.visitAnnotationDefault(this);
+    }
 
-        /**
-         * @param defaultValue
-         *            the default value of this methodinfo's annotation
-         */
-        public final void setDefaultValue(ElementValue defaultValue)
-        {
-                default_value = defaultValue;
-        }
+    /**
+     * @param defaultValue the default value of this methodinfo's annotation
+     */
+    public final void setDefaultValue(ElementValue defaultValue) {
+        default_value = defaultValue;
+    }
 
-        /**
-         * @return the default value
-         */
-        public final ElementValue getDefaultValue()
-        {
-                return default_value;
-        }
-
-        @Override
-        public Attribute copy(ConstantPool _constant_pool)
-        {
-                throw new RuntimeException("Not implemented yet!");
-        }
+    /**
+     * @return the default value
+     */
+    public final ElementValue getDefaultValue() {
+        return default_value;
+    }
 
     @Override
-    public final void dump(DataOutputStream dos) throws IOException
-    {
-      super.dump(dos);
-      default_value.dump(dos);
+    public Attribute copy(ConstantPool _constant_pool) {
+        throw new RuntimeException("Not implemented yet!");
+    }
+
+    @Override
+    public final void dump(DataOutputStream dos) throws IOException {
+        super.dump(dos);
+        default_value.dump(dos);
     }
 }
