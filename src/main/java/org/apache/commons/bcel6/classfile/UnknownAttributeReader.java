a7a3f9c3927f4e51bdabd738cadde7b12a50ed56
Sebastian Bazley <sebb@apache.org>
2015-08-10 18:09:19 +0000

    Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/bcel/trunk@1695143 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/bcel6/classfile/UnknownAttributeReader.java
+++ src/main/java/org/apache/commons/bcel6/classfile/UnknownAttributeReader.java
@@ -36,7 +36,7 @@ public interface UnknownAttributeReader {
      *                      that represents the name of the attribute.
      * @param length        The length of the data contained in the attribute. This is written
      *                      into the constant pool and should agree with what the factory expects the length to be.
-     * @param input         This is the data input that the factory needs to read its data from.
+     * @param file          This is the data input that the factory needs to read its data from.
      * @param constant_pool This is the constant pool associated with the Attribute that we are constructing.
      *                      
      * @return The user-defined AttributeReader should take this data and use
