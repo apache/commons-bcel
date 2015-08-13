5464c3b1066dd4646dc9951e7c16a2658989966e
Gary D. Gregory <ggregory@apache.org>
2015-08-13 17:02:36 +0000

    Add missing '@Override' annotations.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/bcel/trunk@1695740 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/bcel6/verifier/tests/TestArrayAccess02Creator.java
+++ src/test/java/org/apache/commons/bcel6/verifier/tests/TestArrayAccess02Creator.java
@@ -45,7 +45,8 @@ public class TestArrayAccess02Creator extends TestCreator {
     _factory = new InstructionFactory(_cg, _cp);
   }
 
-  public void create(OutputStream out) throws IOException {
+  @Override
+public void create(OutputStream out) throws IOException {
     createMethod_0();
     createMethod_1();
     _cg.getJavaClass().dump(out);
