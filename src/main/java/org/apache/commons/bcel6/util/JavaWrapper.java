9f307d3947513e9008eda54b9d9dfdcdf76b4031
Sebastian Bazley <sebb@apache.org>
2015-08-16 07:51:45 +0000

    BCEL-110 Problem with JAXB if the bcel classloader is used; remove the broken ClassLoader class
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/bcel/trunk@1696114 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/bcel6/util/JavaWrapper.java
+++ src/main/java/org/apache/commons/bcel6/util/JavaWrapper.java
@@ -28,8 +28,7 @@ import java.lang.reflect.Modifier;
  * 
  * <pre>java org.apache.commons.bcel6.util.JavaWrapper &lt;real.class.name&gt; [arguments]</pre>
  * 
- * <p>To use your own class loader you can set the "bcel.classloader" system property
- * which defaults to "org.apache.commons.bcel6.util.ClassLoader", e.g., with:</p>
+ * <p>To use your own class loader you can set the "bcel.classloader" system property<p>
  * <pre>java org.apache.commons.bcel6.util.JavaWrapper -Dbcel.classloader=foo.MyLoader &lt;real.class.name&gt; [arguments]</pre>
  *
  * @version $Id$
@@ -43,7 +42,7 @@ public class JavaWrapper {
     private static java.lang.ClassLoader getClassLoader() {
         String s = System.getProperty("bcel.classloader");
         if ((s == null) || "".equals(s)) {
-            s = "org.apache.commons.bcel6.util.ClassLoader";
+            throw new IllegalArgumentException("The property 'bcel.classloader' must be defined");
         }
         try {
             return (java.lang.ClassLoader) Class.forName(s).newInstance();
