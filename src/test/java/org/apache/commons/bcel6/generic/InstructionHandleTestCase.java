b77a0829e2bd2a80d5590ed2cd90163771c34167
Sebastian Bazley <sebb@apache.org>
2015-08-23 20:58:41 +0000

    Start some tests for InstructionHandle/BranchHandle
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/bcel/trunk@1697254 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/commons/bcel6/generic/InstructionHandleTestCase.java
@@ -0,0 +1,38 @@
+package org.apache.commons.bcel6.generic;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InstructionHandleTestCase {
+
+    // Test that setInstruction only allows Instructions that are not BranchInstructions
+
+    @Test(expected=ClassGenException.class)
+    public void testsetInstructionNull() {
+        InstructionHandle ih = InstructionHandle.getInstructionHandle(new NOP());// have to start with a valid non BI
+        Assert.assertNotNull(ih);
+        ih.setInstruction(null);
+        Assert.assertNotNull(ih);
+    }
+
+    @Test
+    public void testsetInstructionI() {
+        InstructionHandle ih = InstructionHandle.getInstructionHandle(new NOP());// have to start with a valid non BI
+        Assert.assertNotNull(ih);
+        ih.setInstruction(new NOP());        
+        Assert.assertNotNull(ih);
+    }
+
+    @Test(expected=ClassGenException.class)
+    public void testsetInstructionnotI() {
+        InstructionHandle ih = InstructionHandle.getInstructionHandle(new NOP());// have to start with a valid non BI
+        Assert.assertNotNull(ih);
+        ih.setInstruction(new GOTO(null));        
+        Assert.assertNotNull(ih);
+    }
+
+    @Test(expected=ClassGenException.class)
+    public void testGetIHnull() {
+        InstructionHandle.getInstructionHandle(null); 
+    }
+}
