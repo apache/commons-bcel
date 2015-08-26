f762083f51340749238d5cdf0ffdd4e8e9b1ff88
Sebastian Bazley <sebb@apache.org>
2015-08-26 10:35:16 +0000

    Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/bcel/trunk@1697875 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/bcel6/generic/INVOKEVIRTUAL.java
+++ src/main/java/org/apache/commons/bcel6/generic/INVOKEVIRTUAL.java
@@ -26,6 +26,9 @@ import org.apache.commons.bcel6.ExceptionConstants;
  * <PRE>Stack: ..., objectref, [arg1, [arg2 ...]] -&gt; ...</PRE>
  *
  * @version $Id$
+ * @see
+ * <a href="http://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.invokevirtual">
+ * The invokevirtual instruction in The Java Virtual Machine Specification</a>
  */
 public class INVOKEVIRTUAL extends InvokeInstruction {
 
