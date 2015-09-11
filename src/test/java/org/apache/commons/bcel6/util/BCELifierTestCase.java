9bbff9dd617324ebe6487a7481a85cb291985293
Sebastian Bazley <sebb@apache.org>
2015-09-11 17:38:33 +0000

    Add a round-trip BCELifier test case
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/bcel/trunk@1702521 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/bcel6/util/BCELifierTestCase.java
+++ src/test/java/org/apache/commons/bcel6/util/BCELifierTestCase.java
@@ -2,11 +2,15 @@ package org.apache.commons.bcel6.util;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
-
 import org.apache.commons.bcel6.classfile.JavaClass;
 import org.junit.Test;
+
 
 public class BCELifierTestCase {
 
@@ -17,6 +21,57 @@ public class BCELifierTestCase {
         assertNotNull(java_class);
         BCELifier bcelifier = new BCELifier(java_class, os);
         bcelifier.start();
+    }
+
+    /*
+     * Dump a class using "javap" and compare with the same class recreated
+     * using BCELifier, "javac", "java" and dumped with "javap"
+     * TODO: detect if JDK present and skip test if not 
+     */
+    @Test
+    public void testJavapCompare() throws Exception {
+        testClassOnPath("target/test-classes/Java8Example.class");
+    }
+
+    private void testClassOnPath(String javaClass) throws Exception {
+        // Get javap of the input class
+        final String initial = exec(null, "javap", "-p", javaClass);
+
+        final File workDir = new File("target");
+        File infile = new File(javaClass);
+        JavaClass java_class = BCELifier.getJavaClass(infile.getName().replace(".class", ""));
+        assertNotNull(java_class);
+        File outfile = new File(workDir,infile.getName().replace(".class", "Creator.java"));
+        FileOutputStream fos = new FileOutputStream(outfile);
+        BCELifier bcelifier = new BCELifier(java_class, fos);
+        bcelifier.start();
+        fos.close();
+        exec(workDir, "javac", "-cp", "classes", outfile.getName());
+        exec(workDir, "java", "-cp", ".:classes", outfile.getName().replace(".java", ""));
+        final String output = exec(workDir, "javap", "-p", infile.getName());
+        assertEquals(initial, output);
+    }
+
+    private String exec(File workDir, String ... args) throws Exception {
+//        System.err.println(java.util.Arrays.toString(args));
+        ProcessBuilder pb = new ProcessBuilder( args );
+        pb.directory(workDir);
+        Process proc = pb.start();
+        BufferedInputStream is = new BufferedInputStream(proc.getInputStream());
+        InputStream es = proc.getErrorStream();
+        proc.waitFor();
+        byte []buff=new byte[2048];
+        int len;
+        while((len=es.read(buff)) != -1) {
+            System.err.print(new String(buff,0,len));
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while((len=is.read(buff)) != -1) {
+            sb.append(new String(buff,0,len));
+        }
+        is.close();
+        return sb.toString();
     }
 
 }
