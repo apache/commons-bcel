f8c429c570116d867e1f7a4cd7fee3f87a2e9ac7
Mark Roberts <markro@cs.washington.edu>
2016-01-06 13:54:54 -0800

    Add a marker class to identify our version of bcel6.

---
+++ src/main/java/org/apache/commons/bcel6/PLSEMarker.java
@@ -0,0 +1,13 @@
+/*
+ * This is a special marker class used to differentiate our special
+ * version of BCEL used for running Daikon/Chicory from any other
+ * version that may be on the classpath. (markro)
+ */
+
+package org.apache.commons.bcel6;
+
+public class PLSEMarker
+{
+  public static final String marker = "PLSE version of BCEL6";
+}
+
