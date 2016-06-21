35b06e09dbebf887afb6ba5191451074496571e5
Gary D. Gregory <ggregory@apache.org>
2016-06-21 22:34:33 +0000

    On Windows, test whatever JRE and JDK are registered in the Windows Registry.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/bcel/trunk@1749617 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/bcel/generic/JDKGenericDumpTestCase.java
+++ src/test/java/org/apache/bcel/generic/JDKGenericDumpTestCase.java
@@ -17,15 +17,19 @@
 
 package org.apache.bcel.generic;
 
+import static com.sun.jna.platform.win32.WinReg.HKEY_LOCAL_MACHINE;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.FileFilter;
 import java.io.InputStream;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
@@ -33,39 +37,76 @@ import org.apache.bcel.classfile.ClassParser;
 import org.apache.bcel.classfile.Code;
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.Method;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
+import com.sun.jna.platform.win32.Advapi32Util;
+
 /**
- * Test that the generic dump() methods work on the JDK classes
- * Reads each class into an instruction list and then dumps
+ * Test that the generic dump() methods work on the JDK classes Reads each class into an instruction list and then dumps
  * the instructions. The output bytes should be the same as the input.
  */
 @RunWith(Parameterized.class)
 public class JDKGenericDumpTestCase {
 
+    private static final String KEY_JDK = "SOFTWARE\\JavaSoft\\Java Development Kit";
+
+    private static final String KEY_JRE = "SOFTWARE\\JavaSoft\\Java Runtime Environment";
+
     @Parameters(name = "{0}")
-    public static Collection<Object[]> data() {
-        return Arrays.asList(findJavaHomes());
+    public static Collection<String> data() {
+        return findJavaHomes();
     }
 
-    private static Object[][] findJavaHomes() {
-        return new Object[][] { { System.getProperty("java.home") } };
+    private static Set<String> findJavaHomes() {
+        if (SystemUtils.IS_OS_WINDOWS) {
+            return findJavaHomesOnWindows();
+        }
+        final Set<String> javaHomes = new HashSet<>(1);
+        javaHomes.add(System.getProperty("java.home"));
+        return javaHomes;
+    }
+
+    private static Set<String> findJavaHomesOnWindows() {
+        Set<String> javaHomes = new HashSet<>();
+        final String[] jreKeys = Advapi32Util.registryGetKeys(HKEY_LOCAL_MACHINE, KEY_JRE);
+        javaHomes = findJavaHomesOnWindows(jreKeys);
+        final String[] jdkKeys = Advapi32Util.registryGetKeys(HKEY_LOCAL_MACHINE, KEY_JDK);
+        javaHomes.addAll(findJavaHomesOnWindows(jdkKeys));
+        return javaHomes;
+    }
+
+    private static Set<String> findJavaHomesOnWindows(final String[] keys) {
+        final Set<String> javaHomes = new HashSet<>(keys.length);
+        for (final String key : keys) {
+            final String javaHome = Advapi32Util.registryGetStringValue(HKEY_LOCAL_MACHINE, KEY_JRE + "\\" + key,
+                    "JavaHome");
+            if (StringUtils.isNoneBlank(javaHome)) {
+                if (new File(javaHome).exists()) {
+                    javaHomes.add(javaHome);
+                }
+            }
+        }
+        return javaHomes;
     }
 
     public JDKGenericDumpTestCase(final String javaHome) {
         this.javaHome = javaHome;
     }
 
-    private String javaHome;
+    private final String javaHome;
 
     @Test
     public void testJDKjars() throws Exception {
         final File[] jars = listJDKjars();
-        for (final File file : jars) {
-            testJar(file);
+        if (jars != null) {
+            for (final File file : jars) {
+                testJar(file);
+            }
         }
     }
 
