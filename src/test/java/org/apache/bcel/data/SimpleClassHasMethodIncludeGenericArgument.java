75b5820070a6aae1a157fdff4001153751ec532d
sam <sam1287@gmail.com>
2016-07-20 16:38:50 +0900

    TestCase - LocalVariableTypeTable(method generic arguments or variables) is not updated.

---
+++ src/test/java/org/apache/bcel/data/SimpleClassHasMethodIncludeGenericArgument.java
@@ -0,0 +1,19 @@
+package org.apache.bcel.data;
+
+import java.util.List;
+
+/**
+ * @author sam
+ */
+public class SimpleClassHasMethodIncludeGenericArgument
+{
+    public static void a(String a1, List<String> a2)
+    {
+    }
+    public static void b(String b1, List b2)
+    {
+    }
+    public static void c(String c1, String c2)
+    {
+    }
+}
