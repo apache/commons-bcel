04a21a2dfb1c95563224b166c21d746d7dda18dc
Mark Roberts <markro49@users.noreply.github.com>
2019-08-30 15:49:53 -0700

    Update signature scanning in Utility to support TypeParameters. (#32)
    * Update signature scanning in Utility to support TypeParameters.
    
    * Add @since tag to new public method

--- src/test/java/org/apache/bcel/classfile/UtilityTestCase.java
+++ src/test/java/org/apache/bcel/classfile/UtilityTestCase.java
@@ -32,6 +32,7 @@ public class UtilityTestCase extends TestCase {
         assertEquals("generic signature",
                 "java.nio.file.attribute.FileAttribute<?>[]",
                 Utility.signatureToString("[Ljava/nio/file/attribute/FileAttribute<*>;"));
+
     // tests for BCEL-286
         assertEquals("generic signature",
                 "boofcv.alg.tracker.tld.TldTracker<boofcv.struct.image.ImageGray<boofcv.struct.image.GrayU8>, boofcv.struct.image.GrayI<boofcv.struct.image.GrayU8>>",
@@ -42,5 +43,28 @@ public class UtilityTestCase extends TestCase {
         assertEquals("generic signature",
                 "com.jme3.util.IntMap<T>.IntMapIterator",
                 Utility.signatureToString("Lcom/jme3/util/IntMap<TT;>.IntMapIterator;"));
+
+    // tests for BCEL-279
+        assertEquals("type parameters signature",
+                "<T extends java.lang.Object>(com.google.common.io.ByteProcessor<T>, int)T",
+                Utility.signatureToString("<T:Ljava/lang/Object;>(Lcom/google/common/io/ByteProcessor<TT;>;I)TT;", false));
+        assertEquals("type parameters signature",
+                "<T extends Object>(com.google.common.io.ByteProcessor<T>, int)T",
+                Utility.signatureToString("<T:Ljava/lang/Object;>(Lcom/google/common/io/ByteProcessor<TT;>;I)TT;", true));
+        assertEquals("type parameters signature",
+                "<M extends java.lang.reflect.AccessibleObject & java.lang.reflect.Member>(M)void",
+                Utility.signatureToString("<M:Ljava/lang/reflect/AccessibleObject;:Ljava/lang/reflect/Member;>(TM;)V"));
+        assertEquals("type parameters signature",
+                "<K1 extends K, V1 extends V>()com.google.common.cache.Weigher<K1, V1>",
+                Utility.signatureToString("<K1:TK;V1:TV;>()Lcom/google/common/cache/Weigher<TK1;TV1;>;"));
+        assertEquals("type parameters signature",
+                "<K1 extends K, V1 extends V>(com.google.common.cache.Weigher<? super K1, ? super V1>)com.google.common.cache.CacheBuilder<K1, V1>",
+                Utility.signatureToString("<K1:TK;V1:TV;>(Lcom/google/common/cache/Weigher<-TK1;-TV1;>;)Lcom/google/common/cache/CacheBuilder<TK1;TV1;>;"));
+        assertEquals("class signature",
+                "<N extends java.lang.Object, E extends java.lang.Object> extends java.lang.Object implements com.google.common.graph.Network<N, E>",
+                Utility.signatureToString("<N:Ljava/lang/Object;E:Ljava/lang/Object;>Ljava/lang/Object;Lcom/google/common/graph/Network<TN;TE;>;", false));
+        assertEquals("class signature",
+                "<K extends Object, V extends Object> extends Object",
+                Utility.signatureToString("<K:Ljava/lang/Object;V:Ljava/lang/Object;>Ljava/lang/Object;"));
     }
 }
