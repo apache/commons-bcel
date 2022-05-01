b24126e1f19b39e9f41908a4c95061bd7cbb8770
Gary Gregory <gardgregory@gmail.com>
2022-05-01 08:39:07 -0400

    Remove unused import.

--- src/test/java/org/apache/bcel/PLSETestCase.java
+++ src/test/java/org/apache/bcel/PLSETestCase.java
@@ -37,8 +37,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.util.stream.Stream;
 
 public class PLSETestCase extends AbstractTestCase
