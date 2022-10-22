fdc70dd3a4ac4a0cb680164bc0bc4150ac2e3d05
Gary David Gregory (Code signing key) <ggregory@apache.org>
2022-10-22 10:33:11 -0400

    Remove unused import

--- src/test/java/org/apache/bcel/AbstractTestCase.java
+++ src/test/java/org/apache/bcel/AbstractTestCase.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 import org.apache.bcel.classfile.AnnotationEntry;
 import org.apache.bcel.classfile.Attribute;
