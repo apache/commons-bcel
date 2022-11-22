f9e50479e78176f6e27cd3d9861428967a974e04
Gary David Gregory (Code signing key) <ggregory@apache.org>
2022-11-22 07:50:07 -0500

    org.apache.bcel.classfile.Signature constructors now throw ClassFormatException on invalid input

--- src/main/java/org/apache/bcel/classfile/Signature.java
+++ src/main/java/org/apache/bcel/classfile/Signature.java
@@ -21,6 +21,7 @@ import java.io.DataInput;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 import org.apache.bcel.Const;
 import org.apache.bcel.util.Args;
@@ -175,6 +176,8 @@ public final class Signature extends Attribute {
     public Signature(final int nameIndex, final int length, final int signatureIndex, final ConstantPool constantPool) {
         super(Const.ATTR_SIGNATURE, nameIndex, Args.require(length, 2, "Signature length attribute"), constantPool);
         this.signatureIndex = signatureIndex;
+        // validate:
+        Objects.requireNonNull(constantPool.getConstantUtf8(signatureIndex), "constantPool.getConstantUtf8(signatureIndex)");
     }
 
     /**
