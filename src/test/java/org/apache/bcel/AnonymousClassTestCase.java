7ab4a0622a7a46fb7c7fcea7bbca5fb391099be7
nbauma109 <nbauma109@github.com>
2022-12-06 08:58:09 +0100

    added test for null class attribute

--- src/test/java/org/apache/bcel/AnonymousClassTestCase.java
+++ src/test/java/org/apache/bcel/AnonymousClassTestCase.java
@@ -19,11 +19,13 @@ package org.apache.bcel;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.apache.bcel.classfile.InnerClass;
 import org.apache.bcel.classfile.InnerClasses;
 import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.data.EmptyClass;
 import org.junit.jupiter.api.Test;
 
 public class AnonymousClassTestCase extends AbstractTestCase {
@@ -51,9 +53,10 @@ public class AnonymousClassTestCase extends AbstractTestCase {
     @Test
     public void testRegularClassInnerClasses() throws ClassNotFoundException {
         final JavaClass clazz = getTestJavaClass(PACKAGE_BASE_NAME + ".data.AnonymousClassTest");
-        InnerClasses innerClasses = clazz.getAttribute(Const.ATTR_INNER_CLASSES);
-        InnerClass[] innerClassArray = innerClasses.getInnerClasses();
+        final InnerClasses innerClasses = clazz.getAttribute(Const.ATTR_INNER_CLASSES);
+        final InnerClass[] innerClassArray = innerClasses.getInnerClasses();
         assertEquals(3, innerClassArray.length);
+        assertNull(Repository.lookupClass(EmptyClass.class).getAttribute(Const.ATTR_INNER_CLASSES));
     }
     
     @Test
