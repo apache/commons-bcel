253455c6a0f570a83662129e77bd1defd3e72520
Gary David Gregory (Code signing key) <ggregory@apache.org>
2023-03-19 08:39:32 -0400

    Javadoc: Convert package.html to package-info.java

---
+++ src/main/java/org/apache/bcel/verifier/package-info.java
@@ -0,0 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/**
+ * BCEL's verifier JustIce is there to help you dump correct Java class files created or modified with BCEL.
+ * <h2>Package Specification</h2>
+ * <p>
+ * This is the top-level package of the JustIce verifier. To actually use it, have a look at the VerifierFactory and Verifier classes.
+ * </p>
+ */
+package org.apache.bcel.verifier;
