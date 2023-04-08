d330a52c4353e8964621ed011b322d3962964705
nbauma109 <nbauma109@github.com>
2023-04-08 08:59:43 +0200

    Added javadoc links to opcodes

--- src/test/java/org/apache/bcel/verifier/tests/JvmOpCodes.java
+++ src/test/java/org/apache/bcel/verifier/tests/JvmOpCodes.java
@@ -17,6 +17,25 @@
 
 package org.apache.bcel.verifier.tests;
 
+import org.apache.bcel.generic.DNEG;
+import org.apache.bcel.generic.DREM;
+import org.apache.bcel.generic.DUP2;
+import org.apache.bcel.generic.DUP2_X1;
+import org.apache.bcel.generic.DUP2_X2;
+import org.apache.bcel.generic.DUP_X2;
+import org.apache.bcel.generic.FADD;
+import org.apache.bcel.generic.FNEG;
+import org.apache.bcel.generic.FREM;
+import org.apache.bcel.generic.FSUB;
+import org.apache.bcel.generic.IUSHR;
+import org.apache.bcel.generic.LAND;
+import org.apache.bcel.generic.LNEG;
+import org.apache.bcel.generic.LOR;
+import org.apache.bcel.generic.LSHL;
+import org.apache.bcel.generic.LUSHR;
+import org.apache.bcel.generic.POP;
+import org.apache.bcel.generic.POP2;
+
 /*
  * A selection of JVM opcodes, each one in a minimal method.
  */
@@ -24,75 +43,129 @@ public class JvmOpCodes {
 
     long l1, l2;
 
+    /**
+     * Tests {@link DUP2_X1}.
+     */
     void dup2x1(String[] s) {
         s[0] += "s"; // Form 1
         l2 = l1 = 1; // Form 2
     }
 
+    /**
+     * Tests {@link DUP2}.
+     */
     long dup2(long a) {
         return a = a + 1;
     }
 
+    /**
+     * Tests {@link DUP2_X2}.
+     */
     long dup2x2(long[] array, int i, long l) {
         return array[i] = l;
     }
 
+    /**
+     * Tests {@link DUP_X2}.
+     */
     int dupx2(int[] array, int i, int l) {
         return array[i] = l;
     }
 
+    /**
+     * Tests {@link LNEG}.
+     */
     long lneg(long a) {
         return -a;
     }
 
+    /**
+     * Tests {@link LOR}.
+     */
     long lor(long a, long b) {
         return a | b;
     }
 
+    /**
+     * Tests {@link LAND}.
+     */
     long land(long a, long b) {
         return a & b;
     }
 
+    /**
+     * Tests {@link LUSHR}.
+     */
     long lushr(long a, long b) {
         return a >>> b;
     }
 
+    /**
+     * Tests {@link IUSHR}.
+     */
     int iushr(int a, int b) {
         return a >>> b;
     }
 
+    /**
+     * Tests {@link LSHL}.
+     */
     long lshl(long a, long b) {
         return a << b;
     }
 
+    /**
+     * Tests {@link FSUB}.
+     */
     float fsub(float a, float b) {
         return a - b;
     }
 
+    /**
+     * Tests {@link FADD}.
+     */
     float fadd(float a, float b) {
         return a + b;
     }
 
+    /**
+     * Tests {@link FREM}.
+     */
     float frem(float a, float b) {
         return a % b;
     }
 
+    /**
+     * Tests {@link FNEG}.
+     */
     float fneg(float a) {
         return -a;
     }
 
+    /**
+     * Tests {@link DREM}.
+     */
     double drem(double a, double b) {
         return a % b;
     }
 
+    /**
+     * Tests {@link DNEG}.
+     */
     double dneg(double a) {
         return -a;
     }
 
+    /**
+     * Tests {@link POP}.
+     */
     void pop() {
         Math.round(0.5f);
     }
 
+    /**
+     * Tests {@link POP2}.
+     */
     void pop2() {
         Math.round(0.5d);
     }
