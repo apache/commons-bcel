af658efa83519dfc248aa99c64375b2e1106fbdf
Gary Gregory <garydgregory@users.noreply.github.com>
2023-04-09 20:42:46 -0400

    Fix Javadoc since tag

--- src/main/java/org/apache/bcel/classfile/EmptyVisitor.java
+++ src/main/java/org/apache/bcel/classfile/EmptyVisitor.java
@@ -313,7 +313,7 @@ public class EmptyVisitor implements Visitor {
     /**
      * Visits a {@link StackMapType} object.
      * @param obj object to visit
-     * @since 6.7.1
+     * @since 6.8.0
      */
     @Override
     public void visitStackMapType(final StackMapType obj) {
