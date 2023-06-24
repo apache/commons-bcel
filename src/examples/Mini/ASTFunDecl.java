99df9a0fa4ef828c4951ee09cb82d5560fea1bff
Gary David Gregory (Code signing key) <ggregory@apache.org>
2023-06-24 13:36:07 -0400

    Spelling

--- src/examples/Mini/ASTFunDecl.java
+++ src/examples/Mini/ASTFunDecl.java
@@ -466,7 +466,7 @@ public class ASTFunDecl extends SimpleNode implements MiniParserTreeConstants {
 
         /*
          * Update entry of this function, i.e. set argument references. The entry is already in there by garantuee, but may be
-         * of wrong type, i.e. the user defined a function 'TRUE', e.g. and 'TRUE' is of type 'Variable'.
+         * of wrong type, i.e. the user-defined a function 'TRUE', e.g. and 'TRUE' is of type 'Variable'.
          */
         try {
             final Function fun = (Function) env.get(name.getName());
