a35914a398f249b14e9b93121aa6ab06a5c2dc39
Gary Gregory <garydgregory@gmail.com>
2024-04-01 10:39:29 -0400

    Avoid NullPointerException after calling org.apache.bcel.classfile.LocalVariableTable.setLocalVariableTable(LocalVariable[]) with null

--- src/main/java/org/apache/bcel/classfile/LocalVariableTable.java
+++ src/main/java/org/apache/bcel/classfile/LocalVariableTable.java
@@ -35,6 +35,8 @@ import org.apache.bcel.util.Args;
  */
 public class LocalVariableTable extends Attribute implements Iterable<LocalVariable> {
 
+    private static final LocalVariable[] EMPTY_ARRAY = {};
+
     private LocalVariable[] localVariableTable; // variables
 
     /**
@@ -63,7 +65,7 @@ public class LocalVariableTable extends Attribute implements Iterable<LocalVaria
      */
     public LocalVariableTable(final int nameIndex, final int length, final LocalVariable[] localVariableTable, final ConstantPool constantPool) {
         super(Const.ATTR_LOCAL_VARIABLE_TABLE, nameIndex, length, constantPool);
-        this.localVariableTable = localVariableTable != null ? localVariableTable : LocalVariable.EMPTY_ARRAY;
+        this.localVariableTable = localVariableTable != null ? localVariableTable : EMPTY_ARRAY;
         Args.requireU2(this.localVariableTable.length, "localVariableTable.length");
     }
 
@@ -162,7 +164,7 @@ public class LocalVariableTable extends Attribute implements Iterable<LocalVaria
     }
 
     public final int getTableLength() {
-        return localVariableTable == null ? 0 : localVariableTable.length;
+        return localVariableTable.length;
     }
 
     @Override
@@ -171,7 +173,7 @@ public class LocalVariableTable extends Attribute implements Iterable<LocalVaria
     }
 
     public final void setLocalVariableTable(final LocalVariable[] localVariableTable) {
-        this.localVariableTable = localVariableTable;
+        this.localVariableTable = localVariableTable != null ? localVariableTable : EMPTY_ARRAY;
     }
 
     /**
