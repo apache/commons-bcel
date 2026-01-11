6e569d6e919fa86de426a09427899108ba02e39f
Gary Gregory <garydgregory@gmail.com>
2026-01-10 23:32:04 -0500

    Javadoc

--- src/main/java/org/apache/bcel/generic/FNEG.java
+++ src/main/java/org/apache/bcel/generic/FNEG.java
@@ -27,6 +27,9 @@ package org.apache.bcel.generic;
  */
 public class FNEG extends ArithmeticInstruction {
 
+    /**
+     * Constructs an FNEG instruction.
+     */
     public FNEG() {
         super(org.apache.bcel.Const.FNEG);
     }
