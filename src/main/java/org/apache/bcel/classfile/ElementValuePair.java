0ecd1dd356016320f47d9c9e8a36ee23497b3fa9
Gary Gregory <garydgregory@gmail.com>
2026-01-10 22:01:21 -0500

    Javadoc

--- src/main/java/org/apache/bcel/classfile/ElementValuePair.java
+++ src/main/java/org/apache/bcel/classfile/ElementValuePair.java
@@ -36,29 +36,62 @@ public class ElementValuePair {
 
     private final int elementNameIndex;
 
+    /**
+     * Constructs an ElementValuePair.
+     *
+     * @param elementNameIndex the element name index.
+     * @param elementValue the element value.
+     * @param constantPool the constant pool.
+     */
     public ElementValuePair(final int elementNameIndex, final ElementValue elementValue, final ConstantPool constantPool) {
         this.elementValue = elementValue;
         this.elementNameIndex = elementNameIndex;
         this.constantPool = constantPool;
     }
 
+    /**
+     * Dumps this element value pair to a DataOutputStream.
+     *
+     * @param dos the output stream.
+     * @throws IOException if an I/O error occurs.
+     */
     protected void dump(final DataOutputStream dos) throws IOException {
         dos.writeShort(elementNameIndex); // u2 name of the element
         elementValue.dump(dos);
     }
 
+    /**
+     * Gets the name index.
+     *
+     * @return the name index.
+     */
     public int getNameIndex() {
         return elementNameIndex;
     }
 
+    /**
+     * Gets the name string.
+     *
+     * @return the name string.
+     */
     public String getNameString() {
         return constantPool.getConstantUtf8(elementNameIndex).getBytes();
     }
 
+    /**
+     * Gets the value.
+     *
+     * @return the element value.
+     */
     public final ElementValue getValue() {
         return elementValue;
     }
 
+    /**
+     * Gets a short string representation.
+     *
+     * @return a short string representation.
+     */
     public String toShortString() {
         final StringBuilder result = new StringBuilder();
         result.append(getNameString()).append("=").append(getValue().toShortString());
