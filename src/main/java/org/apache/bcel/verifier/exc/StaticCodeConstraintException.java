e189ff935ec79aee4677540e84c0d35309cad7b4
Gary Gregory <garydgregory@gmail.com>
2026-02-14 08:59:58 -0500

    Javadoc

--- src/main/java/org/apache/bcel/verifier/exc/StaticCodeConstraintException.java
+++ src/main/java/org/apache/bcel/verifier/exc/StaticCodeConstraintException.java
@@ -27,6 +27,11 @@ package org.apache.bcel.verifier.exc;
 public abstract class StaticCodeConstraintException extends CodeConstraintException {
     private static final long serialVersionUID = 3858523065007725128L;
 
+    /**
+     * Constructs a StaticCodeConstraintException.
+     *
+     * @param message the exception message.
+     */
     public StaticCodeConstraintException(final String message) {
         super(message);
     }
